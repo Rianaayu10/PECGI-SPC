5ae491c3204f6f1820f5d407678a2b2dfa279cc4
riana.ayu <riana@tos.co.id>
2022-10-24 12:59:09 +0700

    <remove unused file>

--- PECGI_SPC/Class/ClsApprovalSetting.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class ClsApprovalSetting
-    Public Property QELeader1 As String
-    Public Property QELeader2 As String
-    Public Property QESectionHead1 As String
-    Public Property QESectionHead2 As String
-    Public Property CreateUser As String
-    Public Property UpdateUser As String
-End Class
--- PECGI_SPC/Class/clsMachineProcess.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System.Data.SqlClient
-
-Public Class clsMachineProcess
-
-End Class
-
-Public Class clsMachineProcessDB
-
-End Class
