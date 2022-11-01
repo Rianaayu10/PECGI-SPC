732b399b49b291992d39986459e40f3750046dad
riana.ayu <riana@tos.co.id>
2022-11-01 16:29:00 +0700

    <Upd><SSO><Update Logic Login>

--- SSO/SiteHome.Master.vb
+++ SSO/SiteHome.Master.vb
@@ -25,9 +25,6 @@ Public Class SiteHome
 #Region "CONTROL EVENTS"
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
-        MenuIndex = sGlobal.indexMenu
-        IdMenu = sGlobal.idMenu.Trim()
         If SiteTitle = "" Then
             Title = ""
         Else
@@ -35,7 +32,6 @@ Public Class SiteHome
         End If
         PageTitle.InnerText = Title & "SSO SPC SYSTEM"
 
-        ''notif approve
         If IsNothing(Session("user")) Then
             pUser = ""
         Else
@@ -54,55 +50,10 @@ Public Class SiteHome
 
         With Me
             lblUser.Text = Session("user").ToString
-            'lblAdmin.Text = Session("AdminStatus").ToString
         End With
 
         Session.Timeout = 600 'in minutes
     End Sub
-
-#End Region
-
-#Region "FUNCTION"
-
-    'Private Function checkPrivilege() As Boolean
-    '    Dim page As String = Server.HtmlEncode(Request.Path)
-
-    '    Dim resultSplit() As String = page.Split(CChar("/"))
-    '    Dim sqlstring As String = " SELECT * FROM dbo.spc_UserPrivilege UP " &
-    '                              " JOIN dbo.spc_UserMenu UM ON UP.MenuID = UM.MenuID " &
-    '                              " WHERE UP.AllowAccess = 1 AND UserID='" & Session("User").ToString & "' "
-    '    Dim dt As DataTable = sGlobal.GetData(sqlstring)
-    '    If dt.Rows.Count > 0 Then
-    '        Return True
-    '    End If
-
-    '    Return False
-    'End Function
-
-    'Private Sub BindMenu()
-    '    Dim dt As New DataTable
-    '    Dim AdminStatus = Session("AdminStatus").ToString
-
-    '    dt = clsSiteMasterDB.GetMenu(AdminStatus, pUser)
-
-    '    Me.rptMenu.DataSource = dt
-    '    Me.rptMenu.DataBind()
-    'End Sub
-
-    'Public Sub rptMenu_ItemDataBound(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.RepeaterItemEventArgs) Handles rptMenu.ItemDataBound
-    '    Dim dt As New DataTable
-    '    Dim AdminStatus = Session("AdminStatus").ToString
-
-    '    If e.Item.ItemType = ListItemType.Item OrElse e.Item.ItemType = ListItemType.AlternatingItem Then
-    '        Dim rptSubMenu As Repeater = TryCast(e.Item.FindControl("rptChildMenu"), Repeater)
-    '        Dim GroupID As String = DirectCast(e.Item.DataItem, System.Data.DataRowView).Row(0).ToString
-
-    '        dt = clsSiteMasterDB.GetSubMenu(AdminStatus, pUser, IdMenu, MenuIndex.ToString, GroupID)
-
-    '        rptSubMenu.DataSource = dt
-    '        rptSubMenu.DataBind()
-    '    End If
-    'End Sub
 
 #End Region
 
