399383b389bcedf78092ad2efcae9c6e9e83d696
riana.ayu <riana@tos.co.id>
2022-11-22 12:45:13 +0700

    <Upd><ProdSampleVerification><Update Chart>

--- PECGI_SPC/ProdSampleVerification.aspx.vb
+++ PECGI_SPC/ProdSampleVerification.aspx.vb
@@ -1050,14 +1050,14 @@ Public Class ProdSampleVerification
                 LCL.LineStyle.Thickness = 1
                 LCL.LineStyle.DashStyle = DashStyle.Dash
                 diagram.AxisY.ConstantLines.Add(LCL)
-                LCL.AxisValue = Setup.XBarLCL
+                LCL.AxisValue = Setup.CPLCL
 
                 Dim UCL As New ConstantLine("UCL")
                 UCL.Color = System.Drawing.Color.Red
                 UCL.LineStyle.Thickness = 1
                 UCL.LineStyle.DashStyle = DashStyle.Dash
                 diagram.AxisY.ConstantLines.Add(UCL)
-                UCL.AxisValue = Setup.XBarUCL
+                UCL.AxisValue = Setup.CPUCL
 
                 Dim CL As New ConstantLine("CL")
                 CL.Color = System.Drawing.Color.Red
@@ -1065,6 +1065,22 @@ Public Class ProdSampleVerification
                 CL.LineStyle.DashStyle = DashStyle.Dash
                 diagram.AxisY.ConstantLines.Add(CL)
                 CL.AxisValue = Setup.CPCL
+
+                If ChartType = "1" Or ChartType = "2" Then
+                    Dim XBarLCL As New ConstantLine("XBarLCL")
+                    XBarLCL.Color = System.Drawing.Color.Yellow
+                    XBarLCL.LineStyle.Thickness = 1
+                    XBarLCL.LineStyle.DashStyle = DashStyle.Dash
+                    diagram.AxisY.ConstantLines.Add(XBarLCL)
+                    XBarLCL.AxisValue = Setup.XBarLCL
+
+                    Dim XBarUCL As New ConstantLine("XBarUCL")
+                    XBarUCL.Color = System.Drawing.Color.Yellow
+                    XBarUCL.LineStyle.Thickness = 1
+                    XBarUCL.LineStyle.DashStyle = DashStyle.Dash
+                    diagram.AxisY.ConstantLines.Add(XBarUCL)
+                    XBarUCL.AxisValue = Setup.XBarUCL
+                End If
 
                 Dim LSL As New ConstantLine("LSL")
                 LSL.Color = System.Drawing.Color.Red
