c01f82511f05fcf91d32227b5459db19cfb57df7
MalikIlman <98869430+MalikIlman@users.noreply.github.com>
2022-11-29 17:06:07 +0700

    <Upd><Web.config><Add Linked Server Config>

--- PECGI_SPC/App_GlobalResources/Sconn.vb
+++ PECGI_SPC/App_GlobalResources/Sconn.vb
@@ -6,4 +6,7 @@
     Public Shared Function IOTConnectionString() As String
         Return ConfigurationManager.ConnectionStrings("IOTSystem").ConnectionString
     End Function
+    Public Shared Function LinkedServerPECGI() As String
+        Return ConfigurationManager.ConnectionStrings("LinkedServerPECGI").ConnectionString
+    End Function
 End Class
