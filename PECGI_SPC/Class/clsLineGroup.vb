50bd8c05501f16147c96ebd1610afa09368d4fbd
Ari <ari@tos.co.id>
2022-12-15 10:37:22 +0700

    <New><class><add Process Group, Line Group, Process>

---
+++ PECGI_SPC/Class/clsLineGroup.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Data.SqlClient
+Public Class clsLineGroup
+    Public Property FactoryCode As String
+    Public Property ProcessGroup As String
+    Public Property LineGroup As String
+    Public Property LineGroupName As String
+
+End Class
+
+Public Class clsLineGroupDB
+    Public Shared Function GetList(UserID As String, FactoryCode As String, ProcessGroup As String) As List(Of clsLineGroup)
+        Using Cn As New SqlConnection(Sconn.Stringkoneksi)
+            Cn.Open()
+            Dim cmd As New SqlCommand("sp_SPC_FillCombo", Cn)
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Parameters.AddWithValue("UserID", UserID)
+            cmd.Parameters.AddWithValue("FactoryCode", FactoryCode)
+            cmd.Parameters.AddWithValue("ProcessGroup", ProcessGroup)
+            Dim rd As SqlDataReader = cmd.ExecuteReader
+            Dim LineGroupList As New List(Of clsLineGroup)
+            Do While rd.Read
+                Dim LineGroup As New clsLineGroup
+                LineGroup.FactoryCode = rd("FactoryCode")
+                LineGroup.ProcessGroup = rd("ProcessGroup")
+                LineGroup.LineGroup = rd("LineGroup")
+                LineGroup.LineGroupName = rd("ProcessGroupName")
+                LineGroupList.Add(LineGroup)
+            Loop
+            rd.Close()
+            Return LineGroupList
+        End Using
+    End Function
+End Class
