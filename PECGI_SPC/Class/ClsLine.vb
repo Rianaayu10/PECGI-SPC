26071757a724ed4e0779fc86b8877d7d309ef58d
Ari <ari@tos.co.id>
2022-12-19 17:00:06 +0700

    <Upd><clsLine><Get List by Process Code>

--- PECGI_SPC/Class/ClsLine.vb
+++ PECGI_SPC/Class/ClsLine.vb
@@ -54,4 +54,46 @@ Public Class ClsLineDB
             Return FactoryList
         End Using
     End Function
+
+    Public Shared Function GetListByProcess(UserID As String, FactoryCode As String, ProcessCode As String) As List(Of ClsLine)
+        Using Cn As New SqlConnection(Sconn.Stringkoneksi)
+            Cn.Open()
+            Dim q As String = "select distinct L.FactoryCode, L.ProcessCode, L.LineCode, L.LineCode + ' - ' + L.LineName as LineName, " & vbCrLf &
+                "isnull(P.AllowShow, 0) AllowShow, " & vbCrLf &
+                "isnull(P.AllowUpdate, 0) AllowUpdate, " & vbCrLf &
+                "isnull(P.AllowVerify, 0) AllowVerify " & vbCrLf
+            q = q &
+                "from MS_Line L inner join spc_ItemCheckByType I " & vbCrLf &
+                "on L.FactoryCode = I.FactoryCode and L.LineCode = I.LineCode " & vbCrLf &
+                "inner join spc_UserLine P on L.LineCode = P.LineCode " & vbCrLf &
+                "where P.UserID = @UserID and P.AllowShow = 1 " & vbCrLf &
+                "and I.ActiveStatus = 1 " & vbCrLf
+            If FactoryCode <> "" Then
+                q = q & "and L.FactoryCode = @FactoryCode "
+            End If
+            If ProcessCode <> "" Then
+                q = q & "and L.ProcessCode = @ProcessCode "
+            End If
+            q = q & "order by LineCode"
+            Dim cmd As New SqlCommand(q, Cn)
+            cmd.Parameters.AddWithValue("UserID", UserID)
+            cmd.Parameters.AddWithValue("FactoryCode", FactoryCode)
+            cmd.Parameters.AddWithValue("ProcessCode", ProcessCode)
+            Dim rd As SqlDataReader = cmd.ExecuteReader
+            Dim FactoryList As New List(Of ClsLine)
+            Do While rd.Read
+                Dim Factory As New ClsLine
+                Factory.FactoryCode = rd("FactoryCode")
+                Factory.ProcessCode = rd("ProcessCode")
+                Factory.LineCode = rd("LineCode")
+                Factory.LineName = rd("LineName")
+                Factory.AllowShow = rd("AllowShow")
+                Factory.AllowUpdate = rd("AllowUpdate")
+                Factory.AllowVerify = rd("AllowVerify")
+                FactoryList.Add(Factory)
+            Loop
+            rd.Close()
+            Return FactoryList
+        End Using
+    End Function
 End Class
--- PECGI_SPC/Class/clsItemType.vb
+++ PECGI_SPC/Class/clsItemType.vb
@@ -29,4 +29,29 @@ Public Class clsItemTypeDB
             Return ItemTypeList
         End Using
     End Function
+
+    Public Shared Function GetList(FactoryCode As String, LineCode As String, UserID As String) As List(Of clsItemType)
+        Using Cn As New SqlConnection(Sconn.Stringkoneksi)
+            Cn.Open()
+            Dim q As String = "select distinct I.ItemTypeCode, M.Description " & vbCrLf &
+                "From spc_ItemCheckByType I inner Join MS_ItemType M on I.ItemTypeCode = M.ItemTypeCode  " & vbCrLf &
+                "inner join spc_UserLine UL on I.LineCode = UL.LineCode and UL.AllowUpdate = 1 " & vbCrLf &
+                "Where I.FactoryCode = @FactoryCode and ActiveStatus = 1 " & vbCrLf &
+                "and UL.UserID = @UserID and I.LineCode = @LineCode "
+            Dim cmd As New SqlCommand(q, Cn)
+            cmd.Parameters.AddWithValue("FactoryCode", FactoryCode)
+            cmd.Parameters.AddWithValue("UserID", UserID)
+            cmd.Parameters.AddWithValue("LineCode", LineCode)
+            Dim rd As SqlDataReader = cmd.ExecuteReader
+            Dim ItemTypeList As New List(Of clsItemType)
+            Do While rd.Read
+                Dim ItemType As New clsItemType
+                ItemType.ItemTypeCode = rd("ItemTypeCode") & ""
+                ItemType.Description = rd("Description") & ""
+                ItemTypeList.Add(ItemType)
+            Loop
+            rd.Close()
+            Return ItemTypeList
+        End Using
+    End Function
 End Class
